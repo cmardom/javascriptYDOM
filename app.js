ac4ca25967c5f7452fbaaad4d325baa3bae503e0
Carmen <carmenmartnromo@gmail.com>
2023-10-09 17:36:15 +0200

    Ejercicio 1 mejorado

--- app.js
+++ app.js
@@ -1,35 +1,30 @@
 //Ejercicio1
-const boton = document.getElementById('boton');
+document.getElementById('botonEjercicio1').addEventListener('click', ejercicio1);
 
-boton.addEventListener('click', ejecutarPrompt1);
 /*Funcion ejecutarPrompt1: pide los dos numeros
 * - los guarda en divs
 * - los compara
 * - muestra resultado
 * - muestra imagen*/
+function createDivWithText(text){
+    const div = document.createElement('div');
+    div.innerText = text;
+    div.classList.add('col');
+    return div;
+}
+function ejercicio1() {
+    const contenedor = document.querySelector('#contenido-dinamico-ejercicio-1');
+    contenedor.innerHTML = '';
 
-function ejecutarPrompt1() {
     const entrada1 = prompt('Introduce un número');
+    contenedor.append(createDivWithText(entrada1));
+
     const entrada2 = prompt('Introduce otro número');
-
-    const ultimoDiv = document.querySelector('#ej1 > div:last-child');
-
-    const num1 = document.createElement('div');
-    num1.innerText = entrada1;
-    num1.classList.add('col');
-    ultimoDiv.insertAdjacentElement('beforebegin', num1);
-
-    const num2 = document.createElement('div');
-    num2.innerText = entrada2;
-    num2.classList.add('col');
-    ultimoDiv.insertAdjacentElement('beforebegin', num2);
+    contenedor.append(createDivWithText(entrada2));
 
     const resultado = entrada1 > entrada2 ? " El primer número es mayor" : " El segundo número es mayor";
-    const divResultado = document.createElement('div');
-    divResultado.innerText = resultado;
-    divResultado.classList.add('col');
-    ultimoDiv.insertAdjacentElement('beforebegin', divResultado);
 
+    contenedor.append(createDivWithText(resultado));
 }
 
 
