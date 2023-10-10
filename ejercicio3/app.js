ce810352035ede17477f686af667c20b2969bd17
Carmen <cmardom599@g.educaand.es>
2023-10-10 11:54:22 +0200

    ej3 empezado

---
+++ ejercicio3/app.js
@@ -0,0 +1,8 @@
+let listaAnimales = ["https://www.pngegg.com/es/png-dkoze",
+    "https://www.pngegg.com/es/png-dktit",
+    "https://www.pngegg.com/es/png-ovusi",
+    "https://www.pngegg.com/es/png-dkuoo",
+    "https://www.pngegg.com/es/png-btqvg",
+    "https://www.pngegg.com/es/png-dkipd",
+    "https://www.pngegg.com/es/png-dxjil",
+    "https://www.pngegg.com/es/png-pdifa"]
